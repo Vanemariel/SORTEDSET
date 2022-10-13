f2a6628eb1357751636c5c7fb3a64e6756923b6a
vanesa mariel herrera <vanesaailin@hotmail.com>
2022-10-13 08:14:33 -0300

    Agregar archivos de proyecto.

---
+++ SORTEDSET/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SORTEDSET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            SortedSet<string> Diccionario = new SortedSet<string>();
+
+            Console.WriteLine("Ingrese frase al alzar. Si ya no desea ingresar mas frases, no escriba nada y de ENTER.");
+            frase = Console.ReadLine();
+            Diccionario.Add(frase);
+
+            while (frase != "")
+            {
+                frase = Console.ReadLine();
+
+                Diccionario.Add(frase);
+
+                if (Diccionario.Contains(frase))
+                {
+                    Console.WriteLine("La frase ingresada, fue escrita anteriormente");
+                }
+                else
+                {
+                    Console.WriteLine("La frase ingresada, NO fue escrita anteriormente");
+                }
+
+            }
+
+            if (frase == "")
+            {
+                Console.WriteLine("No ha escrito ninguna frase.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
